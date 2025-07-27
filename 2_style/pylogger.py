35252b96ded50bbda78a92762b60833e5b4f4243
reidvfx33 <152922289+reidvfx33@users.noreply.github.com>
2025-07-27 17:17:26 -0400

    DONE 2_style

--- 2_style/pylogger.py
+++ 2_style/pylogger.py
@@ -1,31 +1,30 @@
 # STYLE ***************************************************************************
-# content = assignment
-#
-# date    = 2025-03-07
-# email   = contact@alexanderrichtertd.com
+# content  = Find the stack frame of the caller so that we can note the source
+#            file name, line number and function name.
+# original = logging.init.py
+# warnings = On some versions of IronPython, currentframe() returns None
+#            if IronPython isn't run with -X:Frames.
+# date     = 2025-03-07
+# email    = contact@alexanderrichtertd.com
 #************************************************************************************
 
-# original: logging.init.py
 
-def findCaller(self):
-    """
-    Find the stack frame of the caller so that we can note the source
-    file name, line number and function name.
-    """
-    f = currentframe()
-    #On some versions of IronPython, currentframe() returns None if
-    #IronPython isn't run with -X:Frames.
-    if f is not None:
-        f = f.f_back
-    rv = "(unknown file)", 0, "(unknown function)"
-    while hasattr(f, "f_code"):
-        co = f.f_code
-        filename = os.path.normcase(co.co_filename)
+def findCaller(self): 
+    cFrame = currentframe() 
+    if cFrame is not None:
+        cFrame = cFrame.f_back
+    
+    callerInfo = "(unknown file)", 0, "(unknown function)"
+    while hasattr(cFrame, "f_code"):
+        currentOpen = cFrame.f_code
+        filename = os.path.normcase(currentOpen.co_filename)
+
         if filename == _srcfile:
-            f = f.f_back
+            cFrame = cFrame.f_back
             continue
-        rv = (co.co_filename, f.f_lineno, co.co_name)
-        break
-    return rv
 
-# How can we make this code better?
+        callerInfo = (currentOpen.co_filename, cFrame.f_lineno, currentOpen.co_name)
+        break
+    
+    return callerInfo
+
