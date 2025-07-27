e5bc78773f951fc82d99a99752de8d37a8dd3b58
reidvfx33 <152922289+reidvfx33@users.noreply.github.com>
2025-07-27 16:59:17 -0400

    DONE 2_Style

--- 5_app/SSM_UI.py
+++ 5_app/SSM_UI.py
@@ -1,3 +1,18 @@
+# STYLE ***************************************************************************
+# content = UI setup for single sided mesh creation
+#
+# date    = 2025-07-20
+#
+#need changes/functions:
+#   all need buttons and elements show on screen but is not intuitive or visually pleasing
+#   buttons for added outer faces and showing list 
+#   buttons are non-functional 
+#   need to collected selected faces/objects to corresponding buttons
+#   need to make collected info global for other modules to use
+#   see if best to use getter/setter functions or use global variables
+#**********************************************************************************
+
+
 import os
 import re
 
@@ -12,21 +27,24 @@ class SSM_UI():
         self.outerFaces = []
 
     def ui_elem(self, args = None):
+        """
+        Creates all UI elements and calls function for button press
+        
+        arguments: 
+            None
+        returns: 
+            nothing
+        """
         if cmds.window(self.winName, exists=True):
             cmds.deleteUI(self.winName)
         win = cmds.window(self.winName, title = self.winTitle)
         winFormat = cmds.formLayout()
 
-
         #buttons
         self.objNameButton = cmds.textFieldButtonGrp(label='Object Name:', text='', buttonLabel='Load Selected',w = 490, bc = self.getObjName)
         
         self.addInnerFaces = cmds.button(label='Add Selected Object', w = 120, h = 30)
-        
-        
-        #self.addInnerFaces = cmds.button(label='Add Selected Faces', w = 120, h = 30)
         self.innerFacesList = cmds.cmdScrollFieldExecuter(width=200, height=100)
-        
         
         #window formatting
         cmds.formLayout( winFormat, edit=True, attachForm=[(self.objNameButton, 'top', 120), (self.addInnerFaces, 'top', 220), (self.addInnerFaces, 'left', 220), (self.innerFacesList, 'top', 220),(self.innerFacesList, 'left', 360)])
@@ -34,6 +52,16 @@ class SSM_UI():
         cmds.window(self.winName, edit=True, widthHeight = (1000, 600))
 
     def getObjName(self):
+        """
+        Arguments: 
+            None
+        Returns:
+            name of selected object
+        """
         print("obj name")
+
+    def start(self):
+        self.ui_elem()
+
 d = SSM_UI()
-d.ui_elem()
+d.start()
--- 5_app/getVertexInfo.py
+++ 5_app/getVertexInfo.py
@@ -1,5 +1,17 @@
-#################################################
-#convert selected faces into vertices and get world location of each vertices 
+# STYLE ***************************************************************************
+# content = convert selected faces into vertices and get world location of each vertices 
+#           then find matching faces/vertices and move duplicated mesh to be evenly between the inner and outer faces
+# date    = 2025-07-25
+#
+#needed changes/additions(delete later):
+#   get object name, innerFaces, and outerFaces from SSM_UI
+#   create separate function for getting number from vertex name
+#       or easier/cleaner way to select corresponding vert of duplicated geo
+#   create clearer order of operations
+#   simplify nested for loops for finding mirrored faces/verts
+#**********************************************************************************
+
+
 
 import os
 import re
@@ -14,16 +26,28 @@ class vertexInfo():
         
     
     def faceToVert(self, faces):
+        """
+        Convert list of faces into list of corresponding vertices
+            Reformat vertices list to meet standard naming convention
+
+        Arguments:
+            faces = [list of faces names]
+
+        Returns:
+            [list of [list of 4 connected vertices]]
+            [[x1, x2, x3, x4], [y1 , y2, y3, y4]]
+        """
         verts = []
         for face in faces:
             verts.append(cmds.polyListComponentConversion(face, ff = True, tv = True))
         newVerts = []
-        #objNCompile = face.split('[')[0]
         objName = face.split('.f')[0]
+
+        #break up into individual verts
 
         for vert in verts:
             for v in vert:
-                #break up into individual verts
+                #correct naming convention
                 if ":" in v:  
                     nPCompile = re.compile(r'(\d+)') 
                     numbers = nPCompile.findall(v)
@@ -34,7 +58,18 @@ class vertexInfo():
                     newVerts.append(v)
         return newVerts
 
+    #takes in a single inner face name and compares against outerFaces
     def getMirrorFace(self, face):
+        """
+        Finds corresponding outer face to the given inner face. 
+        The corresponding outer and inners faces will be nearly equal on 2 planes
+
+        Arguments:
+            face: inner face 
+
+        Returns:
+            matchFace: name of mirrored face that matches argument
+        """
         faceLoc = cmds.xform(face, bb = True, q = True)
         smallestDiff = .5
         matchFace = ""
@@ -48,16 +83,32 @@ class vertexInfo():
                 if(diffN < .5):
                     match += 1
                     diff += diffN
+                # if 3 elements have been examined and only 1 matches it cannot be mirrored face
+                # exit the loop    
+                if(i == 3 & match < 2): 
+                    i = 5    
                 if(match == 4):
                     if(diff < smallestDiff):
                         matchFace = oFace
                         smallestDiff = diff
-                elif(i > 3 & match < 2):
-                    i = 5
+                
                 i+=1
         return matchFace
 
+    #arg1 : 1 vert from an inner face
+    #arg2: list of all vert of the corresponding outer face
     def findMirrorVert(self, v, vertsList):
+        """
+        Compares location of two vertices to find which two are the closest to being mirror across 2 axises 
+        
+        Arguments: 
+            v : 1 vert from an inner face
+            vertsList : list of all vert of the corresponding outer face
+        
+        Returns:
+            matchVert : name from matching vert within vertsList
+        """
+        #similar to findMirrorFace but use tranform instead of bounding box
         vLoc = cmds.xform(v, t = True, q = True)
         smallestDiff = .5
 
@@ -81,11 +132,25 @@ class vertexInfo():
         return matchVert
     
     def moveVert(self, mirroredVerts):
+        """
+        Within for loop- for each inner vert
+          1.find mirrored vert
+          2. find {x,y,z} of the spot equidistant between the matching verts
+          3. get the number of the inner vertex
+          4. select corresponding vertex on duplicated geo  //currently hardcoded will need to be variable
+          5. transform duplicated geo vert
+        
+        Arguments:
+            mirredVerts = list of 8 verts. 0-3 belong to innerface and 4-7 belong to outer face
+        
+        Returns:
+            nothing
+        """
         inner = [mirroredVerts[0], mirroredVerts[1], mirroredVerts[2], mirroredVerts[3]]
         outer = [mirroredVerts[4], mirroredVerts[5], mirroredVerts[6], mirroredVerts[7]]
         for v in inner:
             match = self.findMirrorVert(v, outer)
-            newLoc = self.makeGeo(v, match)
+            newLoc = self.findCenter(v, match)
             nPCompile = re.compile(r'(\d+)') 
             number = nPCompile.findall(v)[0]
             dupObject = "military_uniform3.vtx[" + number + "]"
@@ -93,7 +158,18 @@ class vertexInfo():
             cmds.xform(t = newLoc)
 
 
-    def makeGeo(self, innerv, outerv):
+
+    def findCenter(self, innerv, outerv):
+        """
+        Find the point in space equidistant to two given vertices
+
+        Arguments:
+            innerv: vertex from an inner face
+            outerv: vertex from outer face
+
+        Returns:
+            list of x, y, and z coordinates
+        """
         inner =cmds.xform(innerv, t = True, q = True)
         outer = cmds.xform(outerv, t = True, q = True)
 
@@ -117,6 +193,9 @@ class vertexInfo():
         return([x, y, z])
                 
     def test(self):
+        """
+        is currently tested on geo that can be downloaded at https://www.turbosquid.com/3d-models/military-uniform-2418050
+        """
         self.outerFaces = ['military_uniform.f[11548]', 'military_uniform.f[11617]', 'military_uniform.f[12530]','military_uniform.f[12146]', 'military_uniform.f[12347]', 'military_uniform.f[12349]', ]
 
         mirrored = ['military_uniform.f[11218]']
